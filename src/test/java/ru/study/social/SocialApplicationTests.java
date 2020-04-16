1587f47656398a83c096b1b048f2fab8078c3d93
Волков Дмитрий Сергеевич <dsvolkov@phoenixit.ru>
2020-04-16 10:46:41 +0300

    init project

---
+++ src/test/java/ru/study/social/SocialApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
